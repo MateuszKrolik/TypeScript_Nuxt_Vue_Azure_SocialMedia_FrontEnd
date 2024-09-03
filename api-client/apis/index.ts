6adfa4af4031144c0120cd067d7f5fdf7233a89b
Mateusz <mateuszkrolik87@gmail.com>
2024-09-03 13:24:31 +0200

    0_OpenAPI_Nuxt_Vuetify_Setup

---
+++ api-client/apis/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ImageApi';
+export * from './PlacesApi';
+export * from './UsersApi';
+export * from './WebApplication1Api';
---
+++ api-client/models/index.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AccessTokenResponse';
+export * from './ForgotPasswordRequest';
+export * from './HttpValidationProblemDetails';
+export * from './InfoRequest';
+export * from './InfoResponse';
+export * from './LoginRequest';
+export * from './Place';
+export * from './PlaceLocation';
+export * from './PlaceUrlDTO';
+export * from './PlacesResponseDTO';
+export * from './RefreshRequest';
+export * from './RegisterRequest';
+export * from './ResendConfirmationEmailRequest';
+export * from './ResetPasswordRequest';
+export * from './TwoFactorRequest';
+export * from './TwoFactorResponse';
+export * from './UserDTO';
+export * from './UsersResponseDTO';
